cde8b1a0208a401c9dcc5a585d5336e4d8f054ae
aian <53809101+deeowemez@users.noreply.github.com>
2023-07-20 19:05:31 +0800

    add feature to adjust the size of the trail

--- script.js
+++ script.js
@@ -38,13 +38,42 @@ function getRandomColor(){
     return `rgb(${red}, ${green}, ${blue})`;
 }
 
+function changePixel(e){
+    // let pixelSize = 0;
+    console.log(e.target.id);
+    if (e.target.id === "trailSizePlus"){
+        currentPixelSize += 20;
+    } else if (e.target.id === "trailSizeMinus") {
+        currentPixelSize -= 20;
+    } else currentPixelSize = 5;
+    console.log(currentPixelSize);
+}
+
+function mousemoveEventListener(e){
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixel.style.left = (e.clientX - trail.offsetLeft) + 'px';
+    pixel.style.top = (e.clientY - trail.offsetTop) + 'px';
+    pixel.style.backgroundColor = getRandomColor();
+    pixel.style.position = 'absolute';
+    pixel.style.width = `${currentPixelSize}px`;
+    pixel.style.height = pixel.style.width;
+
+    trail.appendChild(pixel);
+  
+    setTimeout(function() {
+        pixel.remove();
+    }, 800);
+}
 
 function hoverIn(e, size, padding){
-    console.log(e.target.id)    
     const divElement = document.querySelector(`#${e.target.id}`);
-    console.log(divElement)
+    // console.log(e.target.id)    
+    // console.log(divElement)
     divElement.style.cssText = `box-sizing: border-box; max-width: ${size}px; max-height: ${size}px; padding: ${padding}px;
-        border: 5px solid ${getRandomColor()} `;    
+        border: 5px solid ${getRandomColor()};`;   
+
+    divElement.addEventListener('mousemove', (e) => mousemoveEventListener(e))
 }
 
 function hoverOut(e, size, padding){    
@@ -64,4 +93,12 @@ function newGrid(){
 }
 
 const btn = document.querySelector('#btn');
+const trail = document.getElementById('trail');
+let currentPixelSize = 5;
 btn.onclick = () => newGrid();
+
+const plusbtn = document.querySelector('#trailSizePlus');
+const minusbtn = document.querySelector('#trailSizeMinus');
+
+plusbtn.onclick = (e) => changePixel(e);
+minusbtn.onclick = (e) => changePixel(e);
